a2def4f0cf5f01cdbcceb29c9b7525f77942a60b
priyanb <priyanb250@gmail.com>
2026-03-16 21:44:48 +0530

    updated serverjs

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("Issue Tracker API Running");
 
+app.get("/",(req,res) => {
+  res.send("Issue Tracker API running");
 });
-app.listen(5000,()=>{
-    console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 5000 ;
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`);
 })
