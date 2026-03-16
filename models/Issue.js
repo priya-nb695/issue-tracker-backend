22b86a3657fd97574b16d091614d4fc9c56b04dc
priyanb <priyanb250@gmail.com>
2026-03-16 21:54:30 +0530

    added Issue model

---
+++ models/Issue.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const issueSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["Open","In Progress","Done"],
+       default:"Open"
+    },
+    priority:{
+        type:String,
+        enum:["Low","Medium","High"],
+        default:"Medium"
+    }
+},
+
+
+)
