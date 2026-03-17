b07df7116d4579e5bb5990f406b57796f52d2ec2
priyanb <priyanb250@gmail.com>
2026-03-17 22:02:54 +0530

    updated serverjs to use  issues route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const issueRoutes = require ("./routes/issues");
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.get("/",(req,res) => {
   res.send("Issue Tracker API running");
 });
+
+app.use("/issues",issueRoutes);
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT,() => {
