1ee49122ec10a6d6acde90321822f9b6fc9ba047
priyanb <priyanb250@gmail.com>
2026-03-17 21:58:36 +0530

    started issues route

---
+++ routes/issues.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const Issue = require("../models/Issue");
+
+//Post/issues
+
+router.post("/",async (req,res) => {
+    try {
+        const issue = new Issue(req.body);
+        const savedIssue = await issue.save();
+
+        res.status(201).json(savedIssue);
+    }
+    catch (error){
+        res.status(500).json({message:error.message});
+    };
+    
+    
+})
