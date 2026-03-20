7188fd15cdeb979307afb827b93aab7b50b6d729
priyanb <priyanb250@gmail.com>
2026-03-20 21:06:24 +0530

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const issueRoutes = require("./routes/issues");
 
@@ -9,6 +10,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
